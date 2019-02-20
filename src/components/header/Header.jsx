870adadc45ddde1ae89c11e3ea854188bb3ec4ec
WWK563388548 <wc563388548@gmail.com>
2019-02-21 01:48:10 +0900

    add header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,11 +1,37 @@
 import React, { Component } from 'react';
-
+import {
+  HeaderWrapper,
+  HeaderContent,
+} from './header_style';
+import logo from './logo.png';
+import reactMovie from './reactMovie_logo.png';
 
 class Header extends Component {
   render() {
     return (
       <div>
-        Header
+        <HeaderWrapper>
+          <HeaderContent>
+            <img 
+              style={{
+                width: '300px',
+                marginTop: '20px',
+                float: 'left',
+              }}
+              src={reactMovie}
+              alt="the main logo"
+            />
+            <img 
+              style={{
+                width: '122px',
+                marginTop: '25px',
+                float: 'right',
+              }}
+              src={logo}
+              alt="movie DB logo"
+            />
+          </HeaderContent>
+        </HeaderWrapper>
       </div>
     );
   }
---
+++ src/components/header/header_style.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const HeaderWrapper = styled.div`
+    width: 100%;
+    height: auto;
+    background: #1c1c1c;
+    padding: 0 20px;
+    box-sizing: border-box; 
+`;
+
+export const HeaderContent = styled.div`
+    max-width: 1280px;
+    min-height: 120px;
+    height: auto;
+    padding: 20px 0px;
+    margin: 0 auto;
+    box-sizing: border-box;
+    overflow: hidden;
+`;
