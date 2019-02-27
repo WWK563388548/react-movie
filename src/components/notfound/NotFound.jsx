f3780c2841d10145f21148f5dd201412f5f22eaa
WWK563388548 <wc563388548@gmail.com>
2019-02-27 22:43:26 +0900

    add Page of NotFound

---
+++ src/components/notfound/NotFound.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div>
+            <h1>This page does not exist</h1>
+        </div>
+    );
+}
+
+export default NotFound;
