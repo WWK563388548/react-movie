6148394e775ac4a6245d89585c9df54790a0a3d1
WWK563388548 <wc563388548@gmail.com>
2019-03-08 22:28:50 +0900

    update component of Navigation

--- src/components/movie/components/navigation/Navigation.jsx
+++ src/components/movie/components/navigation/Navigation.jsx
@@ -1,11 +1,19 @@
 import React, {Component} from 'react';
+import {NavigationContainer, NavigationContent} from './style';
+import {Link} from 'react-router-dom';
 
 class Navigation extends Component {
     render() {
         return (
-            <div>
-                Navigation
-            </div>
+            <NavigationContainer>
+                <NavigationContent>
+                    <Link to="/">
+                        <p>Home</p>
+                    </Link>
+                    <p>/</p>
+                    <p>{this.props.movie}</p>
+                </NavigationContent>
+            </NavigationContainer>
         );
     }
 }
