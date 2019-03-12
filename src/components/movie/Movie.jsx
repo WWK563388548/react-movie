cdeab96dae8285ea2fa464f64e27f7a94eddf7c1
WWK563388548 <wc563388548@gmail.com>
2019-03-12 09:11:27 +0900

    Update component of Movie

--- src/components/movie/Movie.jsx
+++ src/components/movie/Movie.jsx
@@ -5,7 +5,7 @@ import MovieInfoBar from './components/movieInfoBar/MovieInfoBar';
 import Grid from '../home/components/grid/Grid';
 import Actor from './components/actor/Actor';
 import Spinner from '../home/components/spinner/Spinner';
-import {MovieContainer} from './style';
+import {MovieContainer, MovieGrid} from './style';
 
 // const API_URL = 'https://api.themoviedb.org/3/';
 const api_key = 'API_KEY';
@@ -54,11 +54,24 @@ class Movie extends Component {
     render() {
         return (
             <MovieContainer>
-                <Navigation />
-                <MovieInfo />
-                <MovieInfoBar />
-                
-                <Spinner />
+                {this.state.movies ? 
+                    <div>
+                        <Navigation movie={this.state.movies.original_title} />
+                        <MovieInfo movie={this.state.movies} directors={this.state.directors} />
+                        <MovieInfoBar time={this.state.movies.runtime} budget={this.state.movies.budget} revenue={this.state.movies.revenue} />
+                    </div>
+                : null }
+                {this.state.actors ? 
+                    <MovieGrid>
+                        <Grid header={'Actors'}>
+                            {this.state.actors.map((element, index) => {
+                                return <Actor key={index} actor={element} />
+                            })}
+                        </Grid>
+                    </MovieGrid>
+                    : null}
+                {!this.state.actors && !this.state.loading ? <h1>No Movie Found!</h1> : null}
+                {this.state.loading ? <Spinner /> : null}
             </MovieContainer>
         );
     }
--- src/components/movie/style.js
+++ src/components/movie/style.js
@@ -3,3 +3,9 @@ import styled from 'styled-components';
 export const MovieContainer = styled.div`
     margin-bottom: 100px;
 `;
+
+export const MovieGrid = styled.div`
+    max-width: 1280px;
+    margin: 0 auto;
+    padding: 0 20px;
+`;
