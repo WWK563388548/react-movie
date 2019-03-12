f1264d3dd57f98aaacefc38379aad435a5e9570a
WWK563388548 <wc563388548@gmail.com>
2019-03-12 09:06:20 +0900

    add component of Actor

--- src/components/movie/components/actor/Actor.jsx
+++ src/components/movie/components/actor/Actor.jsx
@@ -1,11 +1,22 @@
 import React, {Component} from 'react';
+import {
+    ActorContainer
+} from './style';
+import NoImage from '../../../home/no_image.jpg';
 
 class Actor extends Component {
     render() {
+        const POSTER_SIZE = "w154";
+        const IMAGE_BASE_URL ='http://image.tmdb.org/t/p/';
         return (
-            <div>
-                Actor
-            </div>
+            <ActorContainer>
+                <img 
+                    src={this.props.actor.profile_path ? `${IMAGE_BASE_URL}${POSTER_SIZE}${this.props.actor.profile_path}` : NoImage}
+                    alt="actorthumb"
+                />
+                <span className="actor-name">{this.props.actor.name}</span>
+                <span className="actor-charactor">{this.props.actor.character}</span>
+            </ActorContainer>
         );
     }
 }
---
+++ src/components/movie/components/actor/style.js
@@ -0,0 +1,37 @@
+import styled from 'styled-components';
+
+export const ActorContainer = styled.div`
+    box-sizing: border-box;
+    img {
+        width: 40%;
+        height: auto;
+        float: left;
+        box-sizing: border-box;
+    }
+    .actor-name {
+        font-size: 22px;
+        color:#fff;
+        float: left;
+        margin: 10px 20px;
+        width: 40%;
+        box-sizing: border-box;
+    }
+
+    .actor-name {
+        font-size: 22px;
+        color:#fff;
+        float: left;
+        margin: 10px 20px;
+        width: 40%;
+        box-sizing: border-box;
+    }
+
+    .actor-charactor {
+        font-size: 18px;
+        color:#fff;
+        float: left;
+        margin: 0 20px 10px 20px;
+        width: 40%;
+        box-sizing: border-box;
+    }
+`;
