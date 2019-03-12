f85d0ca208383aae2008f409c6faab33d389e6e4
WWK563388548 <wc563388548@gmail.com>
2019-03-12 09:10:07 +0900

    add component of MovieInfoBar

--- src/components/movie/components/movieInfoBar/MovieInfoBar.jsx
+++ src/components/movie/components/movieInfoBar/MovieInfoBar.jsx
@@ -1,11 +1,92 @@
 import React, {Component} from 'react';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faClock, faMoneyBillWave, faHandHoldingUsd } from '@fortawesome/free-solid-svg-icons'
+import {
+    InfoBarContainer, InfoBarContent, InfoBarContentColumn
+} from './style';
+library.add(faClock);
+library.add(faMoneyBillWave);
+library.add(faHandHoldingUsd);
+
+const calcTime = (time) => {
+    const hours = Math.floor(time / 60);
+    const mins = time % 60;
+    return `${hours}h ${mins}m`;
+}
+
+const convertMoney = (money) => {
+    var formatter = new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+      minimumFractionDigits: 0,
+    });
+    return formatter.format(money);
+}
 
 class MovieInfoBar extends Component {
+
+    
     render() {
         return (
-            <div>
-                MovieInfoBar
-            </div>
+            <InfoBarContainer>
+                <InfoBarContent>
+                    <InfoBarContentColumn>
+                        <FontAwesomeIcon 
+                            style={{
+                                float: "left",
+                                marginTop: "-4px"
+                            }}
+                            icon="clock"
+                            size="2x"
+                        />
+                        <span 
+                            style={{
+                                padding: "5px 0 0 10px",
+                                float: "left",
+                            }}
+                        >
+                            Running Time: {calcTime(this.props.time)}
+                        </span>
+                    </InfoBarContentColumn>
+                    <InfoBarContentColumn>
+                        <FontAwesomeIcon 
+                            style={{
+                                float: "left",
+                                marginTop: "-4px"
+                            }}
+                            icon="money-bill-wave"
+                            size="2x"
+                        />
+                        <span 
+                            style={{
+                                padding: "5px 0 0 10px",
+                                float: "left",
+                            }}
+                        >
+                            Budget: {convertMoney(this.props.budget)}
+                        </span>
+                    </InfoBarContentColumn>
+                    <InfoBarContentColumn>
+                        <FontAwesomeIcon 
+                            style={{
+                                float: "left",
+                                marginTop: "-4px"
+                            }}
+                            icon="hand-holding-usd"
+                            size="2x"
+                        />
+                        <span 
+                            style={{
+                                padding: "5px 0 0 10px",
+                                float: "left",
+                            }}
+                        >
+                            Revenue: {convertMoney(this.props.revenue)}
+                        </span>
+                    </InfoBarContentColumn>
+                </InfoBarContent>
+            </InfoBarContainer>
         );
     }
 }
---
+++ src/components/movie/components/movieInfoBar/style.js
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+
+export const InfoBarContainer = styled.div`
+    width: 100%;
+    height: 105px;
+    background: #1c1c1c;
+    position: relative;
+    padding: 25px 20px 0px 20px;
+    box-sizing: border-box;
+    font-size: 22px;
+`;
+
+export const InfoBarContent = styled.div`
+    max-width: 1280px;
+    width: 100%;
+    margin: 0 auto;
+    color: #fff;
+`;
+
+export const InfoBarContentColumn = styled.div`
+    float: left;
+    width: 33.33%;
+    box-sizing: border-box;
+    padding: 10px 20px 0 0;
+`;
